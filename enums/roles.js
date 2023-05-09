2af5f7f3fe3244071a91668495d058a2f65c5a60
Martin Malmgren <martin.malmgren94@hotmail.com>
2023-05-09 22:07:49 +0200

    More logic for role command
    Resets the role check when:
    - If more the 10 min since last
    - If 5 have gotten and role

---
+++ enums/roles.js
@@ -0,0 +1,11 @@
+const roles = {
+    1: "Safe lane (1)",
+    2: "Mid (2)",
+    3: "Off lane (3)",
+    4: "Soft support (4)",
+    5: "Hard support (5)"
+}
+
+module.exports = {
+    roles
+}
--- services/roleService/test/roleService.test.js
+++ services/roleService/test/roleService.test.js
@@ -1,7 +1,13 @@
 const { smartRandomizeRole, roles } = require('../roleService');
 
-test('Test smartRandomizeRole', () => {
-    const result = smartRandomizeRole();
-    const validValues = Object.values(roles);
-    expect(validValues).toContain(result);
-})
+test('Testing smartRandomizeRole for a hole team', () => {
+    let history = [];
+    for (let index = 0; index < 20; index++) {
+        const result = smartRandomizeRole();
+        const validValues = Object.values(roles);
+        expect(validValues).toContain(result);
+        expect(history.includes(result)).toBeFalsy()
+        history.push(result)
+        if(history.length === 5) history = [];
+    }
+});
