c26be98ce17208db7997e9f42f750a43c8797064
Martin Malmgren <martin.malmgren94@hotmail.com>
2023-05-25 16:39:51 +0200

    Dota2 config for !teams

--- services/teamService/test/mock.js
+++ services/teamService/test/mock.js
@@ -2,10 +2,12 @@ const teamOfFour = ["Hasse", "Nasse", "Klasse", "Fasse"];
 const teamOfTwo = ["Hasse", "Nasse"];
 const teamOfSix = ["Hasse", "Nasse", "Klasse", "Fasse", "Dasse", "Lasse"];
 const teamOfFifteen = ["a", "b", "c", "d", "e", "Hasse", "Nasse", "Klasse", "Fasse", "Dasse", "f", "g", "h", "j", "l"]
+const teamOfEight = ['Hasse', 'Lasse', 'Hassan', 'Dasse', 'Nasse', 'Amanda', 'Emilia', 'Ellen']
 
 module.exports = {
     teamOfFour,
     teamOfTwo,
     teamOfSix,
-    teamOfFifteen
+    teamOfFifteen,
+    teamOfEight
 };
--- services/teamService/test/teamService.test.js
+++ services/teamService/test/teamService.test.js
@@ -1,9 +1,9 @@
 const { teamRandomizer, buildResponseString } = require("../teamService");
-const { teamOfFour, teamOfTwo, teamOfSix, teamOfFifteen } = require("./mock");
+const { teamOfFour, teamOfTwo, teamOfSix, teamOfFifteen, teamOfEight } = require("./mock");
 
 test('testing senario with 2 teams with 2 players in each', () => {
     const result = teamRandomizer({ numberOfTeams: 2, users: teamOfFour });
-    
+
     expect(result).toHaveLength(2);
     expect(result[0]).toHaveLength(2);
     expect(result[1]).toHaveLength(2);
@@ -12,7 +12,7 @@ test('testing senario with 2 teams with 2 players in each', () => {
 
 test('testing senario with 2 teams with 1 player in each', () => {
     const result = teamRandomizer({ numberOfTeams: 2, users: teamOfTwo });
-    
+
     expect(result).toHaveLength(2);
     expect(result[0]).toHaveLength(1);
     expect(result[1]).toHaveLength(1);
@@ -21,7 +21,7 @@ test('testing senario with 2 teams with 1 player in each', () => {
 
 test('testing senario with 3 teams with 2 player in each', () => {
     const result = teamRandomizer({ numberOfTeams: 3, users: teamOfSix });
-    
+
     expect(result).toHaveLength(3);
     expect(result[0]).toHaveLength(2);
     expect(result[1]).toHaveLength(2);
@@ -33,7 +33,7 @@ test('testing senario with 3 teams with 2 player in each', () => {
 
 test('testing senario with 3 teams with 15 player in each', () => {
     const result = teamRandomizer({ numberOfTeams: 3, users: teamOfFifteen });
-    
+
     expect(result).toHaveLength(3);
     expect(result[0]).toHaveLength(5);
     expect(result[1]).toHaveLength(5);
@@ -48,4 +48,14 @@ test('testing buildingResponseString', () => {
     const result = buildResponseString({ teams });
     expect(result).toBeDefined();
     expect(result).toHaveLength(49);
+})
+
+test('testing the dota2 config with some players amounts', () => {
+    const playerVariations = [teamOfEight, teamOfTwo, teamOfFour];
+    playerVariations.forEach(team => {
+        const teams = teamRandomizer({ numberOfTeams: 2, users: team, options: 'dota2' });
+        teams.forEach(team => {
+            expect(team.length == 1 || team.length == 2 || team.length == 3 || team.length == 5).toBeTruthy();
+        });
+    })
 })
